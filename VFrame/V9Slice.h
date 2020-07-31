ec4c3ed07980c979206028187abf0dabcbc25152
Gamepopper <tim@gamepopper.co.uk>
2020-08-01 00:44:14 +0100

    Extra comments for eventual documentation.

--- VFrame/V9Slice.h
+++ VFrame/V9Slice.h
@@ -1,3 +1,36 @@
+/**
+* @file    VSprite.h
+* @author  Tim Stoddard <tim@gamepopper.co.uk>
+*
+* @section LICENSE
+*
+* MIT License
+*
+* Copyright (c) 2020 Tim Stoddard
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* @section DESCRIPTION
+*
+* Nine Slice Rendering Sprite
+*/
+
 #pragma once
 #include "VObject.h"
 
@@ -7,6 +40,7 @@
 #include <SFML/Graphics/Color.hpp>
 #include <SFML/System/String.hpp>
 
+///A renderable object that slices a texture into nine pieces, four of which are unscalable.
 class V9Slice :	public VObject
 {
 protected:
@@ -78,7 +112,7 @@ public:
 	virtual void Update(float dt) override;
 
 	/**
-	* Renders the sprite.
+	* Renders the 9-slice object.
 	* @param RenderTarget The game's sf::RenderTarget object to render data onto.
 	*/
 	virtual void Draw(sf::RenderTarget& RenderTarget) override;
